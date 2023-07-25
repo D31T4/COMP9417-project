48a7fee1185ce031242a8eba7169f4cb51b3ff0a
D31T4 <chinpokleung123@gmail.com>
2023-07-26 01:51:44 +1000

    fix nri_PE.py

--- models/nri_PE.py
+++ models/nri_PE.py
@@ -1,148 +1,44 @@
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-def compute_graph_stats(adj_mat: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+from nri import MLPEncoder, RNNDecoder, NRITrainingParams, NRI
+
+class LearnableStaticVertexEncoding(nn.Module):
     '''
-    compute graph statistics
-
-    Arguments:
-    ---
-    - adjacency matrix
-    
-    Returns:
-    ---
-    - send edges
-    - recv edges
-    - edge weight
+    learnable positional encoding for vertices in a static graph
     '''
-    V = adj_mat.shape[0]
-    deg = torch.sum(adj_mat, dim=1)
 
-    adj_mat = adj_mat.numpy()
-
-    send_edges, recv_edges = np.where(adj_mat)
-    E = send_edges.shape[0]
-
-    # vertex => edge one-hot vector. True when edge point to node
-    edge_adj: torch.Tensor = torch.zeros((V, E))
-
-    send_edges = torch.tensor(send_edges)
-    recv_edges = torch.tensor(recv_edges)
-
-    for v in range(V):
-        edge_adj[v, :] = torch.eq(recv_edges, v)
-
-    # normalize onehot and we get adjcency matrix
-    # similar to normalization in GCN (D^-0.5 @ A @ D^-0.5), NRI does D^-1 @ A
-    edge_adj = edge_adj / deg[:, None]
-
-    return send_edges, recv_edges, edge_adj
-
-class LearnablePositionalEncoding(nn.Module):
-    def __init__(self, max_len: int, emb_dim: int):
-        super(LearnablePositionalEncoding, self).__init__()
+    def __init__(self, nV: int, emb_dim: int):
+        '''
+        Arguments:
+        ---
+        - nV: no. of vertex in graph
+        - emb_dim: embedding dimension
+        '''
+        super(LearnableStaticVertexEncoding, self).__init__()
         
         # This embedding layer will act as our learnable positional encoding
-        self.positional_embedding = nn.Embedding(max_len, emb_dim)
+        self.positional_embedding = nn.Parameter(torch.empty(nV, emb_dim))
         
         # Initialize the positional embeddings to small values
-        nn.init.uniform_(self.positional_embedding.weight, -0.1, 0.1)
+        nn.init.uniform_(self.positional_embedding, -0.1, 0.1)
     
-    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
-        # Determine the shape and dimensions
-        *batch_dims, seq_len, emb_dim = inputs.shape
-        
-        # Generate positions
-        positions = torch.arange(0, seq_len, dtype=torch.long, device=inputs.device)
-        
-        # Get positional embeddings
-        positional_emb = self.positional_embedding(positions)
-        
-        # Expand positional encoding to match the batch dimensions of the input tensor
-        for _ in batch_dims:
-            positional_emb = positional_emb.unsqueeze(0)
-        
-        return positional_emb.expand(*batch_dims, seq_len, emb_dim)
-
-# Create distinct positional encoding classes for the encoder and decoder
-class LearnablePositionalEncodingEncoder(LearnablePositionalEncoding):
-    pass
-
-class LearnablePositionalEncodingDecoder(LearnablePositionalEncoding):
-    pass
-
-class MLP(nn.Module):
-    def __init__(
-        self, 
-        n_in: int, 
-        n_hid: int, 
-        n_out: int, 
-        do_prob: float = 0.
-    ):
+    def forward(self, b: int) -> torch.Tensor:
         '''
         Arguments:
         ---
-        - n_in: input size
-        - n_hid: hidden layer size
-        - n_out: output size
-        - do_prob: dropout probability
-        '''
-        super(MLP, self).__init__()
-
-        self.model = nn.Sequential(
-            nn.Linear(n_in, n_hid),
-            nn.ELU(inplace=True),
-            nn.Dropout(do_prob),
-            nn.Linear(n_hid, n_out),
-            nn.ELU(inplace=True)
-        )
-
-        self.bn = nn.BatchNorm1d(n_out)
-
-        self.init_weights()
-
-    def init_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Linear):
-                nn.init.xavier_normal_(m.weight.data)
-                m.bias.data.fill_(0.1)
-            elif isinstance(m, nn.BatchNorm1d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-
-    def batch_norm(self, inputs: torch.Tensor):
-        '''
-        Arguments:
-        ---
-        - inputs: Tensor[B, N, S], where B = no. of batch, N = no. of elements. S = dim of state
+        - b: no. of batch
 
         Returns:
         ---
-        - Tensor[B, N, S]
+        - position encoding [B, V, S]
         '''
-        # element-wise batch norm
-        x: torch.Tensor = inputs.view(-1, inputs.size(-1))
-        x = self.bn(x)
-        return x.view(inputs.shape)
-    
-    def forward(self, inputs: torch.Tensor):
-        '''
-        Arguments:
-        ---
-        - inputs: Tensor[B, N, n_in]
+        return self.positional_embedding.repeat(b, 1, 1)
 
-        Returns:
-        ---
-        - output: Tensor[B, N, n_out]
-        '''
-        x = self.model(inputs)
-        return self.batch_norm(x)
-
-class MLPEncoder(nn.Module):
+class MLPEncoderWithPE(MLPEncoder):
     '''
-    An autoregressive encoder with fixed window length
+    `MLPEncoder` augmented with learnable position encoding
     '''
 
     def __init__(
@@ -150,91 +46,26 @@ class MLPEncoder(nn.Module):
         n_in: int, 
         n_hid: int, 
         n_out: int, 
-        state_dim,
         adj_mat: torch.Tensor,
         do_prob: float = 0.
     ):
-        '''
-        Arguments:
-        ---
-        - n_in: input size (prior size = T * S) where T = no. of timesteps and S = dim of node state
-        - n_hid: hidden layer size
-        - n_out: output size
-        - adj_mat: adjacency matrix
-        - do_prob: dropout probability
-        '''
-        super(MLPEncoder, self).__init__()
+        super(MLPEncoderWithPE, self).__init__(
+            n_in, 
+            n_hid, 
+            n_out, 
+            adj_mat,
+            do_prob
+        )
 
-        self.mlp1 = MLP(n_in, n_hid, n_hid, do_prob)
-        self.mlp2 = MLP(n_hid * 2, n_hid, n_hid, do_prob)
-        self.mlp3 = MLP(n_hid, n_hid, n_hid, do_prob)
-        self.mlp4 = MLP(n_hid * 3, n_hid, n_hid, do_prob)
+        self.positional_encoding = LearnableStaticVertexEncoding(adj_mat.size(0), n_hid)
 
-        self.fc_out = nn.Linear(n_hid, n_out)
-
-        self.init_weights()
-
-        self.send_edges, self.recv_edges, self.edge_weights = compute_graph_stats(adj_mat)
-
-        self.positional_encoding = LearnablePositionalEncodingEncoder(n_in, state_dim)
-
-    def init_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Linear):
-                nn.init.xavier_normal_(m.weight.data)
-                m.bias.data.fill_(0.1)
-
-    def edge2node(self, edge_embeddings: torch.Tensor) -> torch.Tensor:
-        '''
-        compute node embeddings by averaging embeddings of connected edges.
-        basically a graph convolution with a ones filter
-        
-        Arguments:
-        ---
-        - edge_embeddings: edge embeddings [B, E, n_hid]
-
-        Returns:
-        ---
-        - node_embeddings: node embeddings [B, V, n_hid]
-        '''
-        # node aggregate from in-edges like GCN
-        return torch.matmul(self.edge_weights, edge_embeddings)
-    
-    def node2edge(self, node_embeddings: torch.Tensor) -> torch.Tensor:
-        '''
-        compute edge embedding by concat sending node and receiving node embeddings
-
-        Arguments:
-        ---
-        - node_embeddings: node embedding [B, V, n_hid]
-
-        Returns:
-        ---
-        - edge embedding [B, E, 2 * n_hid]
-        '''
-        send_embed = node_embeddings[:, self.send_edges, :]
-        recv_embed = node_embeddings[:, self.recv_edges, :]
-        edges = torch.cat([send_embed, recv_embed], dim=2)
-        return edges
-    
     def forward(self, inputs: torch.Tensor):
-        '''
-        Arguments:
-        ---
-        - inputs: Tensor[B, T, V, n_in]; where B = batch size, T = no. of timesteps, V = no. of vertex
-
-        Returns:
-        ---
-        - edge latent: Tensor[B, E, n_out]
-        '''
-        inputs = inputs + self.positional_encoding(inputs)
-    
         # reshape to Tensor[B, V, T * n_in]
         # autoregression
         x = inputs.transpose(1, 2).contiguous().view(inputs.size(0), inputs.size(2), -1)
 
         # convert node state to node embedding: Tensor[B, V, n_hid]
-        x = self.mlp1(x)
+        x = self.mlp1(x) + self.positional_encoding(x.size(0))
 
         # compute edge embedding
         x = self.node2edge(x) # aggregate connected nodes: Tensor[B, E, 2 * n_hid]
@@ -253,12 +84,12 @@ class MLPEncoder(nn.Module):
         # edge latent
         return self.fc_out(x)
     
-class NRITrainingParams:
-    def __init__(self, ground_truth_interval: int):
-        self.ground_truth_interval = ground_truth_interval
-
-class RNNDecoder(nn.Module):
-    def __init__(self, 
+class RNNDecoderWithPE(RNNDecoder):
+    '''
+    `RNNDecoder` augmented with positional encoding
+    '''
+    def __init__(
+        self, 
         n_in: int, 
         edge_types: int, 
         n_hid: int, 
@@ -266,116 +97,17 @@ class RNNDecoder(nn.Module):
         do_prob: float = 0., 
         skip_first: bool = False
     ):
-        '''
-        Arguments:
-        ---
-        - n_in: node state size (input, output)
-        - edge_types: no. of edge types
-        - n_hid: hidden layer size
-        - do_prob: dropout probability
-        - skip_first
-        '''
-        super(RNNDecoder, self).__init__()
+        super(RNNDecoderWithPE, self).__init__(
+            n_in, 
+            edge_types, 
+            n_hid, 
+            adj_mat,
+            do_prob, 
+            skip_first
+        )
 
-        self.msg_fc1 = nn.ModuleList([
-            nn.Linear(2 * n_hid, n_hid) for _ in range(edge_types)
-        ])
+        self.positional_encoding = LearnableStaticVertexEncoding(adj_mat.size(0), n_hid)
 
-        self.msg_fc2 = nn.ModuleList([
-            nn.Linear(n_hid, n_hid) for _ in range(edge_types)
-        ])
-
-        self.msg_out_shape = n_hid
-        self.skip_first_edge_type = skip_first
-
-        # GRU
-        self.hidden_r = nn.Linear(n_hid, n_hid, bias=False)
-        self.hidden_i = nn.Linear(n_hid, n_hid, bias=False)
-        self.hidden_h = nn.Linear(n_hid, n_hid, bias=False)
-
-        # GRU
-        self.input_r = nn.Linear(n_in, n_hid, bias=True)
-        self.input_i = nn.Linear(n_in, n_hid, bias=True)
-        self.input_n = nn.Linear(n_in, n_hid, bias=True)
-
-        self.out_fc1 = nn.Linear(n_hid, n_hid)
-        self.out_fc2 = nn.Linear(n_hid, n_hid)
-        self.out_fc3 = nn.Linear(n_hid, n_in)
-
-        self.dropout_prob = do_prob
-
-        self.send_edges, self.recv_edges, self.edge_weights = compute_graph_stats(adj_mat)
-
-        self.positional_encoding = LearnablePositionalEncodingDecoder(n_in, n_in)
-
-    def single_step_forward(
-        self, 
-        inputs: torch.Tensor, 
-        rel_type: torch.Tensor, 
-        hidden: torch.Tensor
-    ) -> tuple[torch.Tensor, torch.Tensor]:
-        '''
-        Arguments:
-        ---
-        - inputs: Tensor[B, V, S]
-        - rel_type: relation type onehot. Tensor[B, E, etype]
-        - hidden: hidden state of GRU. Tensor[B, V, n_hid]
-
-        Returns:
-        ---
-        - prediction at t + 1
-        - hidden state at t + 1
-        '''
-        inputs = inputs + self.positional_encoding(inputs)
-
-        # node2edge: use hidden state as node embedding
-        receivers = hidden[:, self.recv_edges, :]
-        senders = hidden[:, self.send_edges, :]
-        pre_msg = torch.cat([senders, receivers], dim=-1)
-
-        # edge embedding: Tensor[B, E, S]
-        all_msgs = torch.zeros(pre_msg.size(0), pre_msg.size(1), self.msg_out_shape)
-
-        if inputs.is_cuda:
-            all_msgs = all_msgs.cuda()
-
-        start_idx: int = 1
-        norm = float(len(self.msg_fc2))
-        
-        if self.skip_first_edge_type:
-            start_idx -= 1
-            norm -= 1
-
-        # Run separate MLP for every edge type
-        # sum edge embedding for each edge type
-        for i in range(start_idx, len(self.msg_fc2)):
-            msg = pre_msg * rel_type[:, :, i:(i + 1)]
-
-            msg = F.tanh(self.msg_fc1[i](msg))
-            msg = F.dropout(msg, p=self.dropout_prob)
-            msg = F.tanh(self.msg_fc2[i](msg))
-
-            all_msgs += msg / norm
-
-        # edge2node aggregate
-        agg_msgs = torch.matmul(self.edge_weights, all_msgs)
-
-        # GRU-style gated aggregation
-        r = F.sigmoid(self.input_r(inputs) + self.hidden_r(agg_msgs))
-        i = F.sigmoid(self.input_i(inputs) + self.hidden_i(agg_msgs))
-        n = F.tanh(self.input_n(inputs) + r * self.hidden_h(agg_msgs))
-        hidden = (1 - i) * n + i * hidden
-
-        # Output MLP
-        pred = F.dropout(F.relu(self.out_fc1(hidden)), p=self.dropout_prob)
-        pred = F.dropout(F.relu(self.out_fc2(pred)), p=self.dropout_prob)
-        pred = self.out_fc3(pred)
-
-        # Predict position/velocity difference
-        pred = inputs + pred
-
-        return pred, hidden
-    
     def forward(
         self, 
         data: torch.Tensor, 
@@ -384,37 +116,15 @@ class RNNDecoder(nn.Module):
         hidden: torch.Tensor = None,
         train_params: NRITrainingParams = None
     ):
-        '''
-        Arguments:
-        ---
-        - data: tensor[B, T, V, S]
-        - rel_type: relation type onehot. tensor[B, E, etype]
-
-        Returns:
-        ---
-        - predictions: tensor[B, T, V, S]
-        '''
         if hidden is None:
-            hidden = torch.zeros(data.size(0), data.size(2), self.msg_out_shape)
+            hidden = self.positional_encoding(data.size(0))
 
             if data.is_cuda:
                 hidden = hidden.cuda()
 
-        pred_all = []
+        return super().forward(data, rel_type, pred_steps, hidden, train_params)
 
-        for step in range(0, pred_steps):
-            if step == 0 or (self.training and train_params and not step % train_params.ground_truth_interval):
-                ins = data[:, step, :]
-            else:
-                ins = pred_all[-1]
-
-            pred, hidden = self.single_step_forward(ins, rel_type, hidden)
-            pred_all.append(pred)
-
-        preds = torch.stack(pred_all, dim=1)
-        return preds, hidden
-
-class NRI(nn.Module):
+class NRIWithPE(NRI):
     def __init__(
         self, 
         state_dim: int, 
@@ -425,29 +135,17 @@ class NRI(nn.Module):
         hid_dim: int = 256,
         gumbel_temp: float = 0.5,
     ):
-        '''
-        Arguments:
-        ---
-        - state_dim: node state dimension
-        - prior_steps: no. of prior steps for encoder
-        - adj_mat: adjacency matrix
-        - do_prob: dropout prob.
-        - edge_types: no. of edge types
-        - hid_dim: hidden state dimension
-        - gumbel_temp: temperature parameter for sampler
-        '''
         super(NRI, self).__init__()
 
-        self.encoder = MLPEncoder(
+        self.encoder = MLPEncoderWithPE(
             n_in=prior_steps * state_dim,
             n_hid=hid_dim,
             n_out=edge_types,
             adj_mat=adj_mat,
-            do_prob=do_prob,
-            state_dim=state_dim
+            do_prob=do_prob
         )
 
-        self.decoder = RNNDecoder(
+        self.decoder = RNNDecoderWithPE(
             n_in=state_dim,
             edge_types=edge_types,
             n_hid=hid_dim,
@@ -458,34 +156,8 @@ class NRI(nn.Module):
         self.gumbel_temp = gumbel_temp
         self.prior_steps = prior_steps
 
-    def forward(self, data: torch.Tensor, pred_steps: int, rand: bool = False, train_params: NRITrainingParams = None):
-        '''
-        Arguments:
-        ---
-        - data: tensor[B, T, V, state_dim]
-        - pred_steps: no. of steps to be predicted
-        - rand: sample edge types randomly (only applicable in eval mode)
-        - train_params: training params (only applicable in train mode)
-
-        Returns:
-        ---
-        - predictions: tensor[B, pred_steps, V, state_dim]
-        '''
-        logits = self.encoder(data[:, :self.prior_steps, :])
-
-        if rand or self.training:
-            rel_types = F.gumbel_softmax(logits, tau=self.gumbel_temp, hard=True)
-        else:
-            rel_types = F.softmax(logits, dim=-1)
-            index = rel_types.max(-1, keepdim=True)
-            rel_types = torch.zeros_like(logits).scatter_(-1, index, 1.0)
-
-        out, _ = self.decoder(data, rel_types, pred_steps, train_params=train_params)
-        return out, logits
-
-
 if __name__ == '__main__':
     # run test
     adj_mat = torch.ones((3, 3)) - torch.eye(3)    
-    nri = NRI(state_dim=3, prior_steps=2, adj_mat=adj_mat, edge_types=2, hid_dim=16)
+    nri = NRIWithPE(state_dim=3, prior_steps=2, adj_mat=adj_mat, edge_types=2, hid_dim=16)
     out = nri.forward(torch.randn((1, 2, 3, 3)), pred_steps=1)
